17add82171e81dd7295fff5b951905d860e36df9
ARYA_BARAI <148191636+ARYABARAI123@users.noreply.github.com>
2024-04-27 22:40:41 +0530

    Update README.java

--- README.java
+++ README.java
@@ -1,12 +1,12 @@
-### The Java assignment involves creating an application that extends javafx.application.Application and includes a main() method to launch the JavaFX application. It implements the start() method to configure the JavaFX stage.
+###  The Java assignment involves creating an application that extends javafx.application.Application and includes a main() method to launch the JavaFX application. It implements the start() method to configure the JavaFX stage.
 
-### Within the application, UI components such as text fields (employeeIDField, nameField, ageField, emailField, departmentField) and buttons (createTableButton, deleteTableButton, registerButton, viewEmployeesButton, updateButton) are defined. Actions are bound to these buttons using lambda expressions to trigger corresponding methods.
+###  Within the application, UI components such as text fields (employeeIDField, nameField, ageField, emailField, departmentField) and buttons (createTableButton, deleteTableButton, registerButton, viewEmployeesButton, updateButton) are defined. Actions are bound to these buttons using lambda expressions to trigger corresponding methods.
 
-###  The application interacts with a MySQL database using JDBC for connectivity and executes SQL queries for various operations:
+###   The application interacts with a MySQL database using JDBC for connectivity and executes SQL queries for various operations:
 
-## createEmployeeTable(): Method to create a table for storing employee records in the database.
-## deleteEmployeeTable(): Method to delete the employee table from the database.
-## registerEmployee(): Method to insert new employee information into the database.
-## viewEmployees(): Method to retrieve and display all employees' information from the database.
-## updateEmployeeInfo(): Method to update employee information in the database.
-### Additionally, the application handles exceptions and displays appropriate error messages for invalid operations or database connectivity issues.
+##  createEmployeeTable(): Method to create a table for storing employee records in the database.
+##  deleteEmployeeTable(): Method to delete the employee table from the database.
+##  registerEmployee(): Method to insert new employee information into the database.
+##  viewEmployees(): Method to retrieve and display all employees' information from the database.
+##  updateEmployeeInfo(): Method to update employee information in the database.
+###  Additionally, the application handles exceptions and displays appropriate error messages for invalid operations or database connectivity issues.
